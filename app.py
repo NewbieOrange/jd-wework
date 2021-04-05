825ffa9158fac2d23847d4a8a1bac841066188a5
NewbieOrange <NewbieOrange@users.noreply.github.com>
2021-04-05 16:21:55 +0800

    Minor cleanup

--- app.py
+++ app.py
@@ -63,11 +63,9 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        # logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         logging.info('GET request from %s', str(self.client_address))
         message = self.path.split('?', 1)[0]
         title, content = [unquote(s) for s in message.split('/')[-2:]]
-        # logging.info('Title: %s\nContent: %s\n', title, content)
         self._set_response()
         send_notification(title, content)
 
@@ -87,13 +85,13 @@ def run(server_class=HTTPServer, handler_class=RequestHandler, port=5677):
 
 if __name__ == '__main__':
     env = os.environ
-    redis_host = getattr(env, "REDIS_HOST", "localhost")
-    redis_port = int(getattr(env, "REDIS_PORT", "6379"))
-    redis_pwd = getattr(env, "REDIS_PWD", None)
-    wechat_corp_id = getattr(env, "WECHAT_CORP_ID")
-    wechat_secret = getattr(env, "WECHAT_SECRET")
-    agent_id = getattr(env, "AGENT_ID")
-    image_id = getattr(env, "IMAGE_ID")
+    redis_host = getattr(env, 'REDIS_HOST', 'localhost')
+    redis_port = int(getattr(env, 'REDIS_PORT', '6379'))
+    redis_pwd = getattr(env, 'REDIS_PWD', None)
+    wechat_corp_id = getattr(env, 'WECHAT_CORP_ID')
+    wechat_secret = getattr(env, 'WECHAT_SECRET')
+    agent_id = getattr(env, 'AGENT_ID')
+    image_id = getattr(env, 'IMAGE_ID')
     r = redis.Redis(host=redis_host, port=redis_port, db=0, password=redis_pwd)
     wx_client = WeChatClient(corp_id=wechat_corp_id, secret=wechat_secret, session=RedisStorage(r))
     run()
