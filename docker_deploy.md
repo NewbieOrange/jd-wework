f4661459ee5a6225bd4befd1054f88271d46b152
xe5700 <9338143+xe5700@users.noreply.github.com>
2021-04-07 23:56:40 +0800

    formater mk_qcore.py

---
+++ docker_deploy.md
@@ -0,0 +1,5 @@
+# Docker 部署方式
+## docker run部署方式
+- 先创建一个环境变量文件，把需要添加的变量添加进去，比如/root/jd_wework_env.sh
+- docker run -it -name jd_wework -e :
+## SWARM 部署方式
--- mk_qrcore.py
+++ mk_qrcore.py
@@ -7,7 +7,7 @@ import qrcode
 import requests
 
 _user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
-'Chrome/86.0.4240.111 Safari/537.36'
+              'Chrome/86.0.4240.111 Safari/537.36'
 
 
 def _mk_utc_time():
