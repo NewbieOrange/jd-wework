54decf120fa6f89f4da51802754a93840b03557a
NewbieOrange <NewbieOrange@users.noreply.github.com>
2021-07-11 18:24:08 +0800

    Fix qrcode check_ip error

--- mk_qrcore.py
+++ mk_qrcore.py
@@ -6,8 +6,7 @@ import qrcode
 
 import requests
 
-_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-'Chrome/86.0.4240.111 Safari/537.36'
+_user_agent = 'jdapp;android;10.0.5;11;0393465333165363-5333430323261366;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36'
 
 
 def _mk_utc_time():
